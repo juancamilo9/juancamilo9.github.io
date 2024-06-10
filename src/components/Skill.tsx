78fa8b0f42c7f4037fa0681ddd47f19e468d3971
Juan Camilo Duque <kcamilito@gmail.com>
2024-06-10 15:20:16 -0500

    Modifico tamaño de skills

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -12,12 +12,11 @@ const Skill = ({ skill }: SkillTypeProps) => {
     return (
         <div className="bg-white dark:bg-gray-700 rounded-lg overflow-hidden shadow-2xl">
 
-            <div className="p-4 space-y-2">
+            <div className="p-4 space-y-2 items-center flex justify-center flex-col">
                 <img
                     src={logo}
                     alt={nombre}
-                   
-                    className="w-full object-cover"
+                    className="w-20 items-center"
                 />
                 <h3 className="text-lg font-semibold text-gray-100 text-center">{nombre}</h3>
             </div>
